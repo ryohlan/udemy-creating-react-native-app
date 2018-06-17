6e5f5d321e681943f033d4fad2154340674e15e4
ryohlan <sunsunryohlan@gmail.com>
2018-06-18 08:11:19 +0900

    add .gitignore

--- section5/ios/Pods/Target Support Files/Pods-picture_poster/Pods-picture_poster-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_picture_poster : NSObject
-@end
-@implementation PodsDummy_Pods_picture_poster
-@end
--- section6/ios/Pods/Target Support Files/Pods-twitter_client/Pods-twitter_client-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_twitter_client : NSObject
-@end
-@implementation PodsDummy_Pods_twitter_client
-@end
